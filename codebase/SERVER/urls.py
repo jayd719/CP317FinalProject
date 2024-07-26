4bcf842ae4eb266934b1322490902aedc325726d
JD-MBP <jashansingh29@gmail.com>
2024-07-26 04:23:04 -0400

    ;

--- codebase/SERVER/urls.py
+++ codebase/SERVER/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from homepage.views import *
 from Authentication.views import*
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 
@@ -15,4 +16,8 @@ urlpatterns = [
     path('register/patient/', register_patient, name='register_patient'),
     
     path('register/doctor/', register_doctor, name='register_doctor'),
+    
+    path('sign-in/',LoginView.as_view(template_name="Authentication/signIn.html",redirect_authenticated_user=True),name='signInP'),
+    
+    path('logout/',LogoutView.as_view(template_name="Authentication/logout.html"),name='logout'),
 ]
